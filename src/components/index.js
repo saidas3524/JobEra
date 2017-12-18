76b895692c65b6be50e246a012244aa79745698d
Sai Krishna Dasoju <saidaso@microsoft.com>
2017-12-18 21:04:40 +0530

    changes for spinner

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ export {Register} from "./Register/Register";
 
 export { PrivateRoute} from "./common/PrivateRoute"
 
-export { NonAuthenticatedRoute} from "./common/NonAuthenticatedRoute"
+export { NonAuthenticatedRoute} from "./common/NonAuthenticatedRoute";
+
+export { Spinner} from "./common/spinner";
 
 
-export { LogOut} from "./logout"
+export { LogOut} from "./logout";
 
 
 
--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
-import { BrowserRouter, Switch, Route,Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { getStore } from './getStore';
 import App from "./components/app";
 
-import  Login  from "./components/login";
+import Login from "./components/login";
 
 
-import { Error,PrivateRoute,LogOut,NonAuthenticatedRoute } from './components';
+import { Error, PrivateRoute, LogOut, NonAuthenticatedRoute, Spinner } from './components';
 import reducers from './reducers';
 
- import Register from './components/Register/Register';
+import Register from './components/Register/Register';
 
 
 const store = getStore();
@@ -24,16 +24,19 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 ReactDOM.render((
     <Provider store={store}>
         <BrowserRouter>
-            <Switch>
-                
-                <NonAuthenticatedRoute exact path="/Register" component={Register} />
-                <NonAuthenticatedRoute exact path="/Login" component={Login}/>
-                <PrivateRoute exact path="/" component={App} />
-                <PrivateRoute exact path="/logout" component={LogOut} />
+            <div>
+                <Spinner />
+                <Switch>
 
-                <Route component={Error}/>
+                    <NonAuthenticatedRoute exact path="/Register" component={Register} />
+                    <NonAuthenticatedRoute exact path="/Login" component={Login} />
+                    <PrivateRoute exact path="/" component={App} />
+                    <PrivateRoute exact path="/logout" component={LogOut} />
 
-            </Switch>
+                    <Route component={Error} />
+
+                </Switch>
+            </div>
         </BrowserRouter>
     </Provider>
 ), document.querySelector('.root'));
